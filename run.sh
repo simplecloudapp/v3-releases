3322638d57f8b1ead7c53d9c495b0f08a214c2c4
David <david@cappell.net>
2024-10-06 19:36:34 +0200

    refactor: provide potential fixes

--- run.sh
+++ run.sh
@@ -22,20 +22,20 @@ start_screen_if_not_running() {
 
 # Define your sessions and commands here
 auth_secret_path="$SCRIPT_DIR/.secrets/auth.secret"  # Adjust this path or variable as needed
-libs_path="$SCRIPT_DIR/libs"
+libs_dir="libs"
 
 controller_session="simplecloud-controller"
-controller_dir="$SCRIPT_DIR/controller"
+controller_dir="controller"
 controller_group_path="$SCRIPT_DIR/groups"
 controller_launcher="app.simplecloud.controller.runtime.launcher.LauncherKt"
-controller_cmd="java -XX:+UseG1GC -XX:MaxGCPauseMillis=50 -XX:CompileThreshold=100 -XX:+UnlockExperimentalVMOptions -XX:+UseCompressedOops -Xmx512m -Xms256m -cp \"$libs_path/*:.\" $controller_launcher --group-path=\"$controller_group_path\" --auth-secret-path=\"$auth_secret_path\""
+controller_cmd="java -XX:+UseG1GC -XX:MaxGCPauseMillis=50 -XX:CompileThreshold=100 -XX:+UnlockExperimentalVMOptions -XX:+UseCompressedOops -Xmx512m -Xms256m -cp \"$libs_dir/*:$controller_dir/*\" $controller_launcher --group-path=\"$controller_group_path\" --auth-secret-path=\"$auth_secret_path\""
 
 serverhost_session="simplecloud-serverhost"
-serverhost_dir="$SCRIPT_DIR/droplets/serverhost"
+serverhost_dir="droplets/serverhost"
 serverhost_running_servers_path="$SCRIPT_DIR/running"
 serverhost_template_path="$SCRIPT_DIR/templates"
 serverhost_launcher="app.simplecloud.droplet.serverhost.runtime.launcher.LauncherKt"
-serverhost_cmd="java -XX:+UseG1GC -XX:MaxGCPauseMillis=50 -XX:CompileThreshold=100 -XX:+UnlockExperimentalVMOptions -XX:+UseCompressedOops -Xmx512m -Xms256m -cp \"$libs_path/*:.\" $serverhost_launcher --libs-path=\"$libs_path\" --running-servers-path=\"$serverhost_running_servers_path\" --template-path=\"$serverhost_template_path\" --auth-secret-path=\"$auth_secret_path\""
+serverhost_cmd="java -XX:+UseG1GC -XX:MaxGCPauseMillis=50 -XX:CompileThreshold=100 -XX:+UnlockExperimentalVMOptions -XX:+UseCompressedOops -Xmx512m -Xms256m -cp \"$libs_dir/*:$serverhost_dir/*\" $serverhost_launcher --libs-path=\"$SCRIPT_DIR/$libs_dir\" --running-servers-path=\"$serverhost_running_servers_path\" --template-path=\"$serverhost_template_path\" --auth-secret-path=\"$auth_secret_path\""
 
 # Start the controller if not already running
 start_screen_if_not_running "$controller_session" "$controller_dir" "$controller_cmd"
