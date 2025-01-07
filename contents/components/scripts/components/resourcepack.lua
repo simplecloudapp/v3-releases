8695bf3696f1869fbc1d9ad77a05d0ed8753688d
Niklas <57962465+niklasnieberler@users.noreply.github.com>
2025-01-07 15:28:42 +0100

    feat: add resourcepack components

---
+++ contents/components/scripts/components/resourcepack.lua
@@ -0,0 +1,7 @@
+local component = require("component")
+
+return component.create({
+    name = "resource-pack",
+    session = "simplecloud-resource-pack",
+    jar = "resource-pack-runtime.jar"
+})
